7545d6502eb081b0f3f84a3b9ba7b18b7614ad93
user <user@192.168.1.19>
2024-07-11 13:01:30 +0530

    FirstScript

---
+++ ZohoCRM/src/test/java/com/crm/testcript/login.java
@@ -0,0 +1,12 @@
+package com.crm.testcript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class login {
+@Test
+public void log() {
+	Reporter.log("welcome",true);
+	
+}
+}
