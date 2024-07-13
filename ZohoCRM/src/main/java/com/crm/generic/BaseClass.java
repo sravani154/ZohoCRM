bcfa82853130b095e7fe6df7bf9644562f7e6596
user <user@192.168.1.19>
2024-07-13 15:30:34 +0530

    HomePage

--- ZohoCRM/src/main/java/com/crm/generic/BaseClass.java
+++ ZohoCRM/src/main/java/com/crm/generic/BaseClass.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
@@ -34,6 +35,9 @@ public class BaseClass  {
 		l.Login(un, pw);
 	
 	}
-	
+	@AfterMethod
+	public void close() {
+		driver.close();
+	}
 
 }
--- ZohoCRM/src/test/java/com/crm/testcript/login.java
+++ ZohoCRM/src/test/java/com/crm/testcript/login.java
@@ -1,5 +1,10 @@
 package com.crm.testcript;
 
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
@@ -8,7 +13,14 @@ import com.crm.generic.BaseClass;
 public class login extends BaseClass {
 @Test
 public void log() {
-	login l=new login();
+	WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+	wait.until(ExpectedConditions.titleContains("Enter"));
+	
+	String etitle="actiTIME - Enter Time-Track";
+	String atitle=driver.getTitle();
+	Assert.assertEquals(etitle, atitle);
+	Reporter.log("HomePage is displayed",true);
+	
 	
 }
 }
