43634c777ade8d4c4de99176c49508b062b69823
user <user@192.168.1.19>
2024-07-14 18:42:23 +0530

    SEARCH

--- ZohoCRM/src/main/java/com/crm/pom/TaskListPage.java
+++ ZohoCRM/src/main/java/com/crm/pom/TaskListPage.java
@@ -1,14 +1,20 @@
 package com.crm.pom;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
 
 import com.crm.generic.FileLib;
 
 public class TaskListPage {
-	
+FileLib f= new FileLib();
+	String cname=f.getExcelData("taskpage", 2, 0);
+	String cdesc=f.getExcelData("taskpage", 2, 1);
 	@FindBy(xpath="//div[text()='Add New']")
 	private WebElement addnewbtn;
 	@FindBy(xpath="//div[text()='+ New Customer']")
@@ -17,12 +23,18 @@ public class TaskListPage {
 	private WebElement customernametb;
 	@FindBy(id="customerLightBox_descriptionField")
 	private WebElement customerdesc;
-	@FindBy(xpath="//*[@id=\"ext-gen83\"]")
+	@FindBy(xpath="//button")
 	private WebElement activecustomer;
 	@FindBy(xpath="//a[text()='Our Company']")
 	private WebElement customertype;
 	@FindBy(xpath="//span[text()='Create Customer']")
 	private WebElement createcustbtn;
+	@FindBy(xpath="//*[@id=\"cpTreeBlock\"]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[1]/div[2]")
+	private WebElement Allcust;
+	
+	@FindBy(xpath="//div[@class='subtitle ellipsis']")
+	private WebElement searchcust;
+
 	
 	public TaskListPage (WebDriver driver) {
 		PageFactory.initElements(driver, this);
@@ -37,16 +49,36 @@ public class TaskListPage {
 	public void setCustomernametb(WebElement customernametb) {
 		this.customernametb = customernametb;
 	}
-	public void clickcustomerbtn() {
-		customerbtn.click();
-
-}
-	public void createcust(String cname,String cdesc) {
-		customernametb.sendKeys(cname);
-		customerdesc.sendKeys(cdesc);
-		customertype.click();
-		activecustomer.click();
-		createcustbtn.click();
+	
+	public WebElement getcustomerdesc() {
+		return customerdesc;
 	}
+	public void setcustomerdesc(String cdesc) {
+		this.customerdesc = customerdesc;
+	}
+	public void clickcust() {
+		customerbtn.click();
+	}
+	public void createcust(String cname,String cdesc) {
+		
+			
+			customernametb.sendKeys(cname );
+			customerdesc.sendKeys(cdesc );
+			activecustomer.click();
+			customertype.click();
+			createcustbtn.click();
+			
+			
+			
+		}
+	public void searchcust() {
+		Allcust.click();
+		
+	}
+		
+		
+		
+	
+	
 	
 }
--- ZohoCRM/src/test/java/com/crm/testcript/CreateCust.java
+++ ZohoCRM/src/test/java/com/crm/testcript/CreateCust.java
@@ -1,5 +1,10 @@
 package com.crm.testcript;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.crm.generic.BaseClass;
@@ -8,19 +13,22 @@ import com.crm.pom.EnterTimeTrackPage;
 import com.crm.pom.TaskListPage;
 
 public class CreateCust extends BaseClass{
-	FileLib f= new FileLib();
-	String cname=f.getExcelData("taskpage", 0, 1);
-	String cdesc=f.getExcelData("taskpage", 1, 1);
+FileLib f= new FileLib();
+	
+	String cname=f.getExcelData("taskpage", 2, 0);
+	String cdesc=f.getExcelData("taskpage", 2, 1);
+	String name=cname;
+	
 	@Test
 	public void createcust() {
 		EnterTimeTrackPage e= new EnterTimeTrackPage(driver);
 		e.clicktaskbtn();
 		TaskListPage t= new TaskListPage(driver);
 		t.clickaddnewbtn();
-		t.clickcustomerbtn();
+		t.clickcust();
 		t.createcust(cname, cdesc);
-	}
-	
-	
-
+			
 }
+}
+			
+
